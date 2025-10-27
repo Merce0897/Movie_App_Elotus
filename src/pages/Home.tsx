4c6a931ad00c0917b600eb7838baadaffea401bb
Tuan Vinh <truongtuanvinh0801@gmail.com>
2025-10-27 14:39:27 +0700

    chore: resolve conflict

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,34 +1,32 @@
 import { useQuery } from "@tanstack/react-query";
-import MovieCard from "../components/movieCard/MovieCard";
+import MovieCard from "../components/MovieCard/MovieCard";
 
 export default function Home() {
-  const url = import.meta.env.VITE_API_SERVER;
-  const apiKey = import.meta.env.VITE_API_KEY;
-  console.log("apiKey", apiKey);
-
   const { isPending, error, data } = useQuery({
     queryKey: ["repoData"],
     queryFn: () =>
-      fetch(`${url}/movie/popular?language=en-US&page=1`, {
-        headers: {
-          Authorization: `Bearer ${apiKey}`,
-          accept: "application/json",
-        },
-      }).then((res) => res.json()),
+      fetch(
+        `${import.meta.env.VITE_API_URL}/movie/popular?language=en-US&page=1`,
+        {
+          headers: {
+            accept: "application/json",
+            Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
+          },
+        }
+      ).then((res) => res.json()),
   });
 
   if (isPending) return "Loading...";
 
   if (error) return "An error has occurred: " + error.message;
   const movies = data.results;
-  console.log("movies", movies);
 
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className="p-2">
+      <h3>Welcome Home!</h3>
       <div>
         {movies.map((movie: MovieCardProps) => (
-          <MovieCard key={movie.id} movie={movie} />
+          <MovieCard movie={movie} key={movie.id} />
         ))}
       </div>
     </div>
